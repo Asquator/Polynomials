3fb510f534882fc31c2df3aa739e404992540b44
asquator <vistalus@protonmail.com>
2021-12-17 17:20:35 -0500

    Minor changes

--- src/extremumPoint.java
+++ src/extremumPoint.java
@@ -8,25 +8,44 @@ public class extremumPoint extends Point
 {
     private boolean _isMin; // minimum/maximum toggles
 
+    /**
+     * Constructs an extremum point from given coordinates and a type
+     * @param x the x coordinate
+     * @param val the value
+     * @param isMin extremum type (minimum / maximum)
+     */
     public extremumPoint(double x, double val, boolean isMin){
         super(x, val);
         _isMin = isMin;
     }
 
+    /**
+     * Constructs an extremum point from a given point and a type
+     * @param point a point to use
+     * @param isMin extremum type
+     */
     public extremumPoint(Point point, boolean isMin){
         super(point);
         _isMin = isMin;
     }
 
 
+    /*
+    *Checks if a given point x that belongs to the given interval is an extremum assuming that the function has constant signs on two sides of the point.
+    *Uses the derivative of a target function to check whether it changes sign at the point.
+    */
     protected static boolean isExtremum(double x, Interval interval, Polynomial derivative){
         double midFirst = (interval.getA() + x) / 2;
         double midSecond = (x + interval.getB()) / 2;
-        if(derivative.calcAt(midFirst) * derivative.calcAt(midSecond) < 0)
+        if(derivative.calcAt(midFirst) * derivative.calcAt(midSecond) < 0) //The derivative changes signs
             return true;
         return false;
     }
 
+    /*
+     *Checks if a given point x that belongs to the given interval is a minimum assuming that the function has constant signs on two sides of the point.
+     *Uses the first derivative test.
+     */
     protected static boolean checkIfMin(double x, Interval interval, Polynomial derivative){
         double midFirst = (interval.getA() + x) / 2;
         double midSecond = (x + interval.getB()) / 2;
@@ -35,19 +54,35 @@ public class extremumPoint extends Point
         return false;
     }
 
-    public static boolean checkIfMax(double x, Interval interval, Polynomial derivative){
+    /*
+     *Checks if a given point x that belongs to the given interval is a maximum assuming that the function has constant signs on two sides of the point.
+     *Uses the first derivative test.
+     */
+        protected static boolean checkIfMax(double x, Interval interval, Polynomial derivative){
         return !checkIfMin(x, interval, derivative);
     }
 
 
+    /**
+     * Returns if a point is a local minimum
+     * @return true if minimum
+     */
     public boolean isMin(){
         return _isMin;
     }
 
+    /**
+     * Returns if a point is a local maximum
+     * @return true if maximum
+     */
     public boolean isMax(){
         return !_isMin;
     }
 
+    /**
+     * Returns a string representation of the extremum point
+     * @return a string representation of the extremum point
+     */
     public String toString(){
         return super.toString() + " local " + (_isMin ? "minimum" : "maximum");
     }
--- src/inflectionPoint.java
+++ src/inflectionPoint.java
@@ -6,11 +6,22 @@
 public class inflectionPoint extends Point{
     private boolean _downToUp; // concavity toggle
 
+    /**
+     * Constructs an inflection point from it's coordinates and concavity change type
+     * @param x the x coordinate
+     * @param val the value
+     * @param downToUp concavity change type
+     */
     public inflectionPoint(double x, double val, boolean downToUp){
         super(x, val);
         _downToUp = downToUp;
     }
 
+    /**
+     * Constructs an inflection point from a point and a concavity change type
+     * @param point a point to use
+     * @param downToUp concavity change type
+     */
     public inflectionPoint(Point point, boolean downToUp){
         super(point);
         _downToUp = downToUp;
@@ -20,11 +31,23 @@ public class inflectionPoint extends Point{
         return extremumPoint.isExtremum(x, interval, secondDerivative);
     }
 
+    /*
+     * These two functions help determine the concavity change type, assuming the second derivative has constant signs on two sides of the point.
+     */
+
     protected static boolean checkIfUpToDown(double x, Interval interval, Polynomial secondDerivative){
         return extremumPoint.checkIfMin(x, interval, secondDerivative);
     }
 
+    protected static boolean checkIfDownToUp(double x, Interval interval, Polynomial secondDerivative){
+        return extremumPoint.checkIfMin(x, interval, secondDerivative);
+    }
 
+
+    /**
+     * Get string representation of the point
+     * @return String representation of the point
+     */
     public String toString(){
         return super.toString() + " point of inflection, concavity changes from " + (_downToUp ? "down to up" : "up to down");
     }
